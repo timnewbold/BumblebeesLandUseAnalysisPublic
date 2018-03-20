a76a03bf399da4f11f7984b1f96800cfba3a645b
Tim Newbold <timnewbold.research@gmail.com>
2018-03-20 10:29:36 +0000

    updates to data preparation code

--- 2_PrepareDiversityData.R
+++ 2_PrepareDiversityData.R
@@ -40,5 +40,68 @@ diversity <- do.call('rbind',lapply(X = split(diversity,diversity$SS),FUN = func
   return(s)
 }))
 
+diversity <- diversity[(diversity$Best_guess_binomial!=""),]
+diversity <- diversity[(diversity$Best_guess_binomial!="Unknown bombus"),]
+
+diversity <- droplevels(diversity)
+
+sp.names <- read.table(paste(dataDir,"bombus_stacknames.txt",sep=""))
+
+TEI_bl <- readRDS(paste(dataDir,"BaselineTEI_Spp.rds",sep=""))
+names(TEI_bl) <- paste("Bombus_",sp.names$x,sep="")
+PEI_bl <- readRDS(paste(dataDir,"BaselinePEI_Spp.rds",sep=""))
+names(PEI_bl) <- paste("Bombus_",sp.names$x,sep="")
+TEI_delta <- readRDS(paste(dataDir,"DeltaTEI_Spp_Period3.rds",sep=""))
+names(TEI_delta) <- paste("Bombus_",sp.names$x,sep="")
+PEI_delta <- readRDS(paste(dataDir,"DeltaPEI_Spp_Period3.rds",sep=""))
+names(PEI_delta) <- paste("Bombus_",sp.names$x,sep="")
+
+diversity <- do.call('rbind',lapply(X = split(x = diversity,f = diversity$Best_guess_binomial),FUN = function(div.sp){
+  
+  sp <- gsub(" ","_",div.sp$Best_guess_binomial[1])
+  
+  if (sp %in% names(TEI_bl)){
+    div.sp$TEI_BL <- raster::extract(x = TEI_bl[[sp]],y = div.sp[,c('Longitude','Latitude')])
+  } else {
+    div.sp$TEI_BL <- NA
+  }
+  
+  if (sp %in% names(PEI_bl)){
+    div.sp$PEI_BL <- raster::extract(x = PEI_bl[[sp]],y = div.sp[,c('Longitude','Latitude')])
+  } else {
+    div.sp$PEI_BL <- NA
+  }
+  
+  if (sp %in% names(TEI_delta)){
+    div.sp$TEI_delta <- raster::extract(x = TEI_delta[[sp]],y = div.sp[,c('Longitude','Latitude')])
+  } else {
+    div.sp$TEI_delta <- NA
+  }
+  
+  if (sp %in% names(PEI_delta)){
+    div.sp$PEI_delta <- raster::extract(x = PEI_delta[[sp]],y = div.sp[,c('Longitude','Latitude')])
+  } else {
+    div.sp$PEI_delta <- NA
+  }
+  
+  return(div.sp)
+}))
+
+diversity$LandUse <- paste(diversity$Predominant_land_use)
+diversity$LandUse[(diversity$LandUse=="Primary vegetation")] <- "Natural"
+diversity$LandUse[(diversity$LandUse=="Mature secondary vegetation")] <- "Natural"
+diversity$LandUse[(diversity$LandUse=="Intermediate secondary vegetation")] <- "Natural"
+diversity$LandUse[(diversity$LandUse=="Young secondary vegetation")] <- "Natural"
+diversity$LandUse[(diversity$LandUse=="Secondary vegetation (indeterminate age)")] <- "Natural"
+diversity$LandUse[(diversity$LandUse=="Cropland")] <- "Human"
+diversity$LandUse[(diversity$LandUse=="Pasture")] <- "Human"
+diversity$LandUse[(diversity$LandUse=="Urban")] <- "Human"
+diversity$LandUse[(diversity$LandUse=="Cannot decide")] <- NA
+diversity$LandUse <- factor(diversity$LandUse,levels=c("Natural","Human"))
+
+diversity$occur <- ifelse(diversity$Measurement>0,1,0)
+
+
+
 cat('Saving diversity data\n')
 save(diversity,file=paste(outDir,"diversity_data.Rd",sep=""))
