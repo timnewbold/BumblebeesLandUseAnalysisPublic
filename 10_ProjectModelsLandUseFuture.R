780d9577e5da4ef6212c22b173dd478030d1648f
Tim Newbold <timnewbold.research@gmail.com>
2018-04-17 17:37:12 +0100

    Added RCP 2.6 scenario to future projections of species-level bumblebee models

--- 10_ProjectModelsLandUseFuture.R
+++ 10_ProjectModelsLandUseFuture.R
@@ -15,6 +15,9 @@ if(Sys.info()['nodename']=='UCBTTNE-PC2'){
   stop("Computer not recognized, so can't find data!")
 }
 
+scenarios <- c("26","85")
+scenario.labels <- c("IMAGE","MESSAGE")
+
 load(paste(modelsDir,"TemperatureModels.rd",sep=""))
 
 bombus.ranges <- stack(paste(dataDir,"bombus_0.tif",sep=""))
@@ -40,75 +43,84 @@ pred_bl <- stack(lapply(bombus.ranges@layers,function(sp){
   
 }))
 
-ExtractRCP(wDir = RCPDir,zipName = "RCP data.zip",scenario = "MESSAGE",years = c(2070))
+invisible(mapply(function(scenario,scenario.label){
+  
+  cat(paste('Projecting future scenario ',scenario.label,' ',scenario,'\n',sep=''))
+  
+  ExtractRCP(wDir = RCPDir,zipName = "RCP data.zip",scenario = scenario.label,years = c(2070))
+  
+  lu_2070 <- get_rcp_grid(wdir = RCPDir,scenario = scenario.label,years = 2070)
+  
+  natural_2070 <- raster(lu_2070$primary[[1]])+raster(lu_2070$secondary[[1]])
+  human_2070 <- raster(lu_2070$cropland[[1]])+raster(lu_2070$pasture[[1]])+raster(lu_2070$urban[[1]])+raster(lu_2070$plantation[[1]])
+  
+  total <- natural_2070 + human_2070
+  
+  values(natural_2070) <- values(natural_2070)/values(total)
+  values(human_2070) <- values(human_2070)/values(total)
+  
+  mask <- natural_2070 + human_2070
+  values(mask)[!is.na(values(mask))] <- 1
+  
+  pred_2070 <- stack(lapply(bombus.ranges@layers,function(sp){
+    
+    new_ras <- sp
+    
+    df <- data.frame(pres=values(sp))
+    
+    df$LandUse <- factor("Natural",levels=levels(m_lu$data$LandUse))
+    df$occur <- 0
+    
+    df$pred <- PredictGLMER(model = m_lu$model,data = df,se.fit = FALSE)$y
+    
+    df$pred <- 1/(1+exp(-(df$pred)))
+    
+    df$pred <- df$pred * df$pres
+    
+    values(new_ras) <- df$pred * values(natural_2070)
+    
+    df$LandUse <- factor("Human",levels=levels(m_lu$data$LandUse))
+    df$occur <- 0
+    
+    df$pred <- PredictGLMER(model = m_lu$model,data = df,se.fit = FALSE)$y
+    
+    df$pred <- 1/(1+exp(-(df$pred)))
+    
+    df$pred <- df$pred * df$pres
+    
+    values(new_ras) <- values(new_ras) + (df$pred * values(human_2070))
+    
+    return(new_ras)
+    
+  }))
+  
+  pred_2070_propn <- sum(pred_2070,na.rm=TRUE)/sum(pred_bl,na.rm=TRUE)
+  
+  values(pred_2070_propn) <- values(pred_2070_propn) * values(mask)
+  
+  brks <- c(0,0.85,0.9,0.95,0.975,1,1.025,1.05,1.1,1.15,9e99)
+  brks[length(brks)] <- max(1.151,max(values(pred_2070_propn),na.rm=TRUE))
+  brks[1] <- min(0.849,min(values(pred_2070_propn),na.rm=TRUE))
+  
+  png(filename = paste(outDir,"2070MapLandUseScenario",scenario,".png",sep=""),
+      width = 17.5,height = 8,units = "cm",res = 1200)
+  
+  par(mar=c(0.5,0.5,0.5,4))
+  
+  plot(pred_2070_propn,breaks=brks,col=brewer.pal(n = length(brks),name = "RdYlBu"),
+       xlim=c(-180,40),ylim=c(10,78),xaxt="n",yaxt="n")
+  
+  text(-160,30,paste("Average\nintactness\n= ",
+                     round(mean(values(pred_2070_propn),na.rm=TRUE)*100,0),"%",sep=""))
+  
+  invisible(dev.off())
+  
+  saveRDS(object = pred_2070_propn,file = paste(
+    outDir,"2070MapLandUseScenario",scenario,".rds",sep=""))
+  saveRDS(object = mask,file = paste(outDir,"Mask.rds",sep=""))
+  
+},scenarios,scenario.labels))
 
-lu_2070 <- get_rcp_grid(wdir = RCPDir,scenario = "MESSAGE",years = 2070)
 
-natural_2070 <- raster(lu_2070$primary[[1]])+raster(lu_2070$secondary[[1]])
-human_2070 <- raster(lu_2070$cropland[[1]])+raster(lu_2070$pasture[[1]])+raster(lu_2070$urban[[1]])+raster(lu_2070$plantation[[1]])
-
-total <- natural_2070 + human_2070
-
-values(natural_2070) <- values(natural_2070)/values(total)
-values(human_2070) <- values(human_2070)/values(total)
-
-mask <- natural_2070 + human_2070
-values(mask)[!is.na(values(mask))] <- 1
-  
-pred_2070 <- stack(lapply(bombus.ranges@layers,function(sp){
-  
-  new_ras <- sp
-  
-  df <- data.frame(pres=values(sp))
-  
-  df$LandUse <- factor("Natural",levels=levels(m_lu$data$LandUse))
-  df$occur <- 0
-  
-  df$pred <- PredictGLMER(model = m_lu$model,data = df,se.fit = FALSE)$y
-  
-  df$pred <- 1/(1+exp(-(df$pred)))
-  
-  df$pred <- df$pred * df$pres
-  
-  values(new_ras) <- df$pred * values(natural_2070)
-  
-  df$LandUse <- factor("Human",levels=levels(m_lu$data$LandUse))
-  df$occur <- 0
-  
-  df$pred <- PredictGLMER(model = m_lu$model,data = df,se.fit = FALSE)$y
-  
-  df$pred <- 1/(1+exp(-(df$pred)))
-  
-  df$pred <- df$pred * df$pres
-  
-  values(new_ras) <- values(new_ras) + (df$pred * values(human_2070))
-  
-  return(new_ras)
-  
-}))
-
-pred_2070_propn <- sum(pred_2070,na.rm=TRUE)/sum(pred_bl,na.rm=TRUE)
-
-values(pred_2070_propn) <- values(pred_2070_propn) * values(mask)
-
-brks <- c(0,0.85,0.9,0.95,0.975,1,1.025,1.05,1.1,1.15,9e99)
-brks[length(brks)] <- max(1.151,max(values(pred_2070_propn),na.rm=TRUE))
-brks[1] <- min(0.849,min(values(pred_2070_propn),na.rm=TRUE))
-
-png(filename = paste(outDir,"2070MapLandUse.png",sep=""),
-    width = 17.5,height = 8,units = "cm",res = 1200)
-
-par(mar=c(0.5,0.5,0.5,4))
-
-plot(pred_2070_propn,breaks=brks,col=brewer.pal(n = length(brks),name = "RdYlBu"),
-     xlim=c(-180,40),ylim=c(10,78),xaxt="n",yaxt="n")
-
-text(-160,30,paste("Average\nintactness\n= ",
-                   round(mean(values(pred_2070_propn),na.rm=TRUE)*100,0),"%",sep=""))
-
-invisible(dev.off())
-
-saveRDS(object = pred_2070_propn,file = paste(outDir,"2070MapLandUse.rds",sep=""))
-saveRDS(object = mask,file = paste(outDir,"Mask.rds",sep=""))
 
 
--- 14_CompareProjectionsFuture.R
+++ 14_CompareProjectionsFuture.R
@@ -6,34 +6,41 @@ luClimDir <- "12_ProjectModelsLandUseAndClimateFuture/"
 
 outDir <- "14_CompareProjectionsFuture/"
 
-luMap <- readRDS(paste(luDir,"2070MapLandUse.rds",sep=""))
-luClimMap <- readRDS(paste(luClimDir,"2070MapLandUseAndClimate.rds",sep=""))
+scenarios <- c("26","85")
 
-diffMap <- luClimMap - luMap
+invisible(sapply(scenarios,function(scen){
+  
+  luMap <- readRDS(paste(luDir,"2070MapLandUseScenario",scen,".rds",sep=""))
+  luClimMap <- readRDS(paste(luClimDir,"2070MapLandUseAndClimateScenario",scen,".rds",sep=""))
+  
+  diffMap <- luClimMap - luMap
+  
+  brks <- c(-1,-0.75,-0.5,-0.25,-0.1,-0.01,0.01,0.1111,1.33333,2,4,10)
+  brks[length(brks)] <- max(values(diffMap),na.rm=TRUE)
+  cols <- c(rev(brewer.pal(n = floor((length(brks)-1)/2),name = "Reds")),
+            "#dddddd",
+            brewer.pal(n = floor((length(brks)-1)/2),name = "Blues"))
+  
+  
+  
+  png(filename = paste(outDir,"DifferenceMapScenario",scen,".png",sep=""),
+      width = 17.5,height = 8,units = "cm",res = 1200)
+  
+  par(mar=c(0.5,0.5,0.5,4))
+  
+  plot(diffMap,breaks=brks,col=cols,xlim=c(-180,40),ylim=c(10,78),xaxt="n",yaxt="n")
+  
+  worse <- round((length(which(values(diffMap) <= -0.01))/length(
+    which(!is.na(values(diffMap)))))*100,0)
+  better <- round((length(which(values(diffMap) >= 0.01))/length(
+    which(!is.na(values(diffMap)))))*100,0)
+  no.change = 100 - worse - better
+  
+  text(-180,45,paste(worse,"% worse"),pos=4)
+  text(-180,40,paste(no.change,"% the same"),pos=4)
+  text(-180,35,paste(better,"% better"),pos=4)
+  
+  invisible(dev.off())
+  
+}))
 
-brks <- c(-1,-0.75,-0.5,-0.25,-0.1,-0.01,0.01,0.1111,1.33333,2,4,10)
-brks[length(brks)] <- max(values(diffMap),na.rm=TRUE)
-cols <- c(rev(brewer.pal(n = floor((length(brks)-1)/2),name = "Reds")),
-         "#dddddd",
-         brewer.pal(n = floor((length(brks)-1)/2),name = "Blues"))
-
-
-
-png(filename = paste(outDir,"DifferenceMap.png",sep=""),
-    width = 17.5,height = 8,units = "cm",res = 1200)
-
-par(mar=c(0.5,0.5,0.5,4))
-
-plot(diffMap,breaks=brks,col=cols,xlim=c(-180,40),ylim=c(10,78),xaxt="n",yaxt="n")
-
-worse <- round((length(which(values(diffMap) <= -0.01))/length(
-  which(!is.na(values(diffMap)))))*100,0)
-better <- round((length(which(values(diffMap) >= 0.01))/length(
-  which(!is.na(values(diffMap)))))*100,0)
-no.change = 100 - worse - better
-
-text(-180,45,paste(worse,"% worse"),pos=4)
-text(-180,40,paste(no.change,"% the same"),pos=4)
-text(-180,35,paste(better,"% better"),pos=4)
-
-invisible(dev.off())
